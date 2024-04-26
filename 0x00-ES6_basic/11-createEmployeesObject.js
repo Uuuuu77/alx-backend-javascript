85453bc21eb57971ada2d45c65e8e2bdcb24b4bb
John Njuguna <johnmnjuguna89@gmail.com>
2024-04-26 17:50:51 +0000

    Iterator

---
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,7 @@
+export default function createEmployeesObject(departmentName, employees) {
+  const employees_object = {
+    [departmentName]: [...employees],
+  };
+
+  return employees_object;
+}
