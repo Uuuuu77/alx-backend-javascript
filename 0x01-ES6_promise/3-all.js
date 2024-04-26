63135672ce3e84e27bf74fbb25c2d5de5dc2f53c
John Njuguna <johnmnjuguna89@gmail.com>
2024-04-26 20:33:00 +0000

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/extensions.
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((result) => {
+      console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
+    })
+    .catch(() => console.error('Signup system offline'));
+}
