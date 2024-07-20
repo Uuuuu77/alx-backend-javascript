e23d4b3bc211b63e25bd2dc8998fcd9cfc1183d9
John Njuguna <johnmnjuguna89@gmail.com>
2024-07-20 14:39:28 +0000

    Basic Integration testing

---
+++ 0x06-unittests_in_js/8-api/api.js
@@ -0,0 +1,17 @@
+// api.js
+
+const express = require('express');
+
+const app = express();
+
+const PORT = 7865;
+
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
+});
+
+app.get('/', (_, res) => {
+  res.send('Welcome to the payment system');
+});
+
+module.exports = app;
---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,39 @@
+// api.test.js
+
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', () => {
+  it('Correct GET/ Response', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      if (error) return done(error);
+      expect(response.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('Correct result?', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      if (error) return done(error);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('Correct response for invalid route?', (done) => {
+    request.get('http://localhost:7865/invalid', (error, response, body) => {
+      if (error) return done(error);
+      expect(response.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+  it('Server should return 404 for invalid route', (done) => {
+    request.get('http://localhost:7865/invalid', (error, response) => {
+      if (error) return done(error);
+      expect(response.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+});
